c0d969377c4252d6a272554baafee4d4d2480925
Josué Hass <50870420+josuehasspereira@users.noreply.github.com>
2022-07-27 00:28:36 -0300

    Adicionado a leitura e armazenamento do dados.json

--- 3 - Distribuidora.c
+++ 3 - Distribuidora.c
@@ -1,48 +1,60 @@
-//Desenvolvido por Josué Hass Pereira, 2022
+//Desenvolvido por Josue Hass Pereira, 2022
 //jozzuhass@gmail.com
 
 #include <stdio.h>
-#include <locale.h>
+#include <string.h>
+#define MAX 1000
 
 int main() {
-	
-	//Definindo acentuação
-    setlocale(LC_ALL, "Portuguese");
 
-    float maior, menor, soma, media, vetor[30] = {22174.1664, 24537.6698, 
-    26139.6134, 0.0, 0.0, 26742.6612, 0.0, 42889.2258, 46251.174, 11191.4722, 
-    0.0, 0.0, 3847.4823, 373.7838, 2659.7563, 48924.2448, 18419.2614, 0.0, 0.0, 
-    35240.182, 43829.1667, 18235.6852, 4355.0662, 13327.1025, 0.0, 0.0, 25681.8318, 
-    1718.1221, 13220.495, 8414.61};
-    int n, i, cont = 0, cont2 = 0, maiDia, menDia;
+	//Abrindo o arquivo dados.json e definindo uma string
+    FILE* f = fopen("dados.json","r");
     
-    //Definindo valor nulo
+	//Definindo variaveis
+	char linha[1001];
+    char s[50];
+    float vetor[MAX];
+    int i, n = 0;
+    float maior, menor, soma, media;
+    int cont = 0, cont2 = 0, maiDia, menDia;
+    
+    //Definindo um Laço do arquivo f para percorrer cada linha
+    while( fgets( linha, 1000, f ) != NULL ){
+    	
+		//Definindo string ('valor') e float 
+		//Percorrendo a linha e definindo o vetor na posição n do laço
+    	if( sscanf( linha, "%s %f", s, &vetor[n]) == 2 ){
+    		
+			//Incrementação do n para o laço
+			if( strstr( s, "valor" ) != NULL ){
+    			n++;
+    		}
+    	}
+	}    
+    fclose( f );
+   
+   	//Definindo valores zeros 
     maior = vetor[0];
     menor = vetor[0];
     soma = 0;
     
-    printf("---Dados de Faturamento diário da Distribuidora X---\n\n");
+    printf("---Dados de Faturamento diario da Distribuidora X---\n\n");
     
-    //Definindo a estrutura de repetição para percorrer o vetor
+    //Laço de repetição paara percorrer o vetor e verificar maior/menor
     for (i = 0; i < 30; i++){
-        
-        //Filtrando com 0 de fatutamento
         if(vetor[i] > 0){
-            
-            //Definição do maior/menor
-            if(vetor[i] < menor){
+			if(vetor[i] < menor){
                 menor = vetor[i];
                 menDia = i + 1;
             } else if (vetor[i] > maior){
                 maior = vetor[i];
                 maiDia = i + 1;
             }
-            
-            //Somatória dos dias
             soma += vetor[i];
             cont += 1;
         }
-        //Definição dos dias a mais que a média
+        
+        //Definindo dias acima da media
         media = soma / cont;
         if(vetor[i] > media){
             cont2 +=1;
@@ -53,6 +65,6 @@ int main() {
     printf("Maior valor de faturamento ocorreu no dia %i: R$ %f\n", maiDia, maior);
     printf("Menor valor de faturamento ocorreu no dia %i: R$ %f\n", menDia, menor);
     printf("Quantidade de dias acima do faturamento mensal: %i\n", cont2);
-
+	
     return 0;
 }
